b5f7a720af86e7093ef6712e3f34ebe3d98d1b04
Vår Aunevik <vaunevik@car13s1.42barcelona.com>
2024-06-10 14:55:21 +0200

    removed some things

--- inc/minishell.h
+++ inc/minishell.h
@@ -30,10 +30,64 @@
 // libft 
 # include "../src/libft/inc/libft.h"
 
-# include "lexer.h"
-# include "parser.h"
-
 # define PROMPT "mish> "
+# define C_LESS '<'
+# define C_GREAT '>'
+# define C_PIPE '|'
+# define C_SQUOTE '\''
+# define C_DQUOTE '"'
+
+/*-----------------Enums------------------*/
+typedef enum e_token
+{
+	T_PIPE = 1,
+	T_REDIR_IN,
+	T_HEREDOC,
+	T_REDIR_OUT,
+	T_APPEND,
+	T_WORD,
+};
+
+typedef enum e_builtin
+{
+    PWD = 1,
+    ECHO,
+    CD,
+    EXPORT,
+    UNSET,
+    ENV,
+    EXIT,
+    NO_BUILTIN,
+};
+
+/*--------------Structures----------------*/
+typedef struct s_lex
+{
+	enum e_token	type; //type of token
+	char			*literal; //the string literal (eg. "cat -e")
+	int				index; //position in the linked list
+	struct s_lex	*next;
+	struct s_lex	*prev;
+}	t_lex;
+
+typedef struct s_parser
+{
+    t_lex   *lexer;
+    t_lex   *redirections;
+    t_data  *data;
+    int     redir_count
+}   t_parser;
+
+typedef struct s_cmds
+{
+    char            **cmd; //the full command with its flags
+    enum e_builtin  builtin; //builtin spesification 
+    int             redir_count; //number of redirections
+    t_lex           *redirs; //structure with redirection information (type of redir and the corresponding file name)
+    t_lex           *lexer; // structure with all the tokens
+    struct s_cmds   *next;
+    struct s_cmds   *prev;
+}   t_cmds;
 
 typedef struct s_data
 {
@@ -47,7 +101,12 @@ typedef struct s_data
     int         err_status;
 }   t_data;
 
+/*------------Main--------------*/
 
-// main
+
+/*-----------Lexer--------------*/
+
+
+/*-----------Parser-------------*/
 
 #endif
