d15018f2dd585848bc033483411867269b2cdb74
Vår Aunevik <varaunevik@MacBook-Pro.local>
2024-06-30 15:14:10 +0200

    updated code

--- src/mini_loop.c
+++ src/mini_loop.c
@@ -34,16 +34,20 @@ void    init_data(t_data *data)
  */
 void    mini_loop(t_data *data)
 {
-    data->input = readline(PROMPT);
-    if (!data->input)
+    char    *tmp;
+
+    tmp = readline(PROMPT);
+    if (!tmp)
         exit(0);
+    data->input = ft_strtrim(tmp, " \t\n\r");
     if (!data->input || !data->input[0])
     {
+        free(tmp);
         free(data->input);
         mini_loop(data);
     }
     add_history(data->input);
-    check_quotes(data); //whether there are any unclosed quotes
+    check_closed_quotes(data); //whether there are any unclosed quotes
     tokenize(data);
     check_tokens(data, &data->lexer); //check whether any error w. redirections (multiple pipes in a row etc. )
     parsing(data);
