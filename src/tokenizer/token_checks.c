57b95a675a29729d751cc9c3ca02cd5869ef0bfa
Vår Aunevik <vaunevik@car13s1.42barcelona.com>
2024-07-18 12:18:16 +0200

    headers

--- src/tokenizer/token_checks.c
+++ src/tokenizer/token_checks.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_checks.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vaunevik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 12:17:41 by vaunevik          #+#    #+#             */
+/*   Updated: 2024/07/18 12:17:44 by vaunevik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int check_redirs(t_lex **lst, t_data *data)
 {
     if (((*lst)->type >= T_REDIR_IN && (*lst)->type <= T_APPEND) && (!(*lst)->next || (*lst)->next->type != T_WORD)
     {
-        data->g_exit = 258;
-        on_error("Error: Syntax error near unexpected token 'newline'", 258, data);
+        data->g_exit = 2;
+        on_error("Minish: syntax error near unexpected token 'newline'", data);
         return (0);
     }
     return (1);
@@ -13,8 +25,8 @@ int check_pipes(t_lex **lst, t_data *data)
 {
     if ((*lst)->type == T_PIPE && ((!(*lst)->next) || ((*lst)->next->type != T_WORD && !((*lst)->next->type >= T_REDIR_IN && (*lst)->next->type <= T_APPEND))))
     {
-        data->g_exit = 258;
-        on_error("Syntax error near unexpected token '|'\n", 127, data);
+        data->g_exit = 2;
+        on_error("Minish: syntax error near unexpected token '|'\n", data);
         return (0);
     }
     return (1);
@@ -26,8 +38,8 @@ int    check_tokens(t_data *data, t_lex **lst)
         return (0);
     if ((*lst)->type == T_PIPE)
     {
-        data->g_exit = 258;
-        on_error("Syntax error near unexpected token '|'", 127, data);
+        data->g_exit = 2;
+        on_error("Minish: syntax error near unexpected token '|'", data);
         return (0);
     }
     while (*lst)
