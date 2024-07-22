7340947a3a5a4701629cd28f3d15ac24fb36d6e1
Vår Aunevik <vaunevik@car10s1.42barcelona.com>
2024-07-22 14:53:14 +0200

    added the hdoc check function to the tokenizer

--- check_tokenizer.c
+++ check_tokenizer.c
@@ -50,11 +50,21 @@ typedef struct s_lex
 	struct s_lex	*next;
 }	t_lex;
 
-t_lex	*tokenizer(char *input);
+typedef struct s_data
+{
+    char        *input; // users input string
+    t_lex       *lexer; // pointer to the linked list of tokens
+    int         hdoc_count; // How many heredocs are present in the input
+    int         *pid; // Pointer to array of the pids
+    int         g_exit; // "global" return error number
+    int         pipes; // NUmber of pipes present to know for how many child processes needed
+}   t_data;
+
+t_lex	*tokenizer(char *input, t_data *data);
 int     token_length(char *input);
 void    add_index(t_lex *tokens);
 void    free_tokens(t_lex *tokens);
-t_lex   *fill_tokens(t_lex *tokens, char *input, int length);
+t_lex   *fill_tokens(t_lex *tokens, char *input, int length, t_data *data);
 int  find_type(char *literal);
 
 
@@ -106,7 +116,45 @@ int token_length(char *input)
     return length;
 }
 
-static t_lex *make_token(int length, char *input)
+int is_hdoc_present(t_lex *tokens)
+{
+    while (tokens)
+    {
+        if (tokens->type == T_HEREDOC)
+            return (1);
+        tokens = tokens->next;
+    }
+    return (0);
+}
+
+void    print_error(int type)
+{
+    if (type == 1)
+        printf("Syntax error near unexpected token '>'");
+    printf("Syntax error near unexpected token '>'\n");
+    return ;
+}
+
+int check_syntax_and_hdoc(t_data *data, t_lex *tokens, char *input, t_lex *new)
+{
+    int i;
+
+    i = 0;
+	if (input[i] == ' ' || (input[i] >= 8 && input[i] <= 13))
+        i++;
+	if (input[i] == '>' || input[i] == '<' || input[i] == '|' || !input[i])
+	{
+		if (is_hdoc_present(tokens))
+		{
+			data->g_exit = 2;
+			print_error(new->type);
+			new->index = -1; //raising flag to check whether we encountered this spesific error
+		}
+	}
+    return (0);
+}
+
+static t_lex *make_token(int length, char *input, t_lex *tokens, t_data *data)
 {
     int i;
     t_lex *new;
@@ -129,6 +177,8 @@ static t_lex *make_token(int length, char *input)
     new->literal[i] = '\0';
     new->next = NULL;
     new->type = find_type(new->literal);
+    if (new->type == T_APPEND || new->type == T_REDIR_IN || new->type == T_HEREDOC || new->type == T_REDIR_OUT || new->type == T_PIPE)
+        check_syntax_and_hdoc(data, tokens, input, new);
     return (new);
 }
 
@@ -148,12 +198,12 @@ void    lex_free(t_lex *lst)
     return ;
 }
 
-t_lex *fill_tokens(t_lex *tokens, char *input, int length)
+t_lex *fill_tokens(t_lex *tokens, char *input, int length, t_data *data)
 {
     t_lex *last;
     t_lex *new;
 
-    new = make_token(length, input);
+    new = make_token(length, input, tokens, data);
     if (!new)
     {
         if (tokens)
@@ -172,6 +222,51 @@ t_lex *fill_tokens(t_lex *tokens, char *input, int length)
     return (tokens);
 }
 
+void    lex_clearone(t_lex **lst)
+{
+    if ((*lst)->literal)
+    {
+        free((*lst)->literal);
+        (*lst)->literal = NULL;
+    }
+    free(*lst);
+    *lst = NULL;
+}
+
+void    lex_delfirst(t_lex **lst)
+{
+    t_lex   *tmp;
+
+    tmp = *lst;
+    *lst = tmp->next;
+    lex_clearone(&tmp);
+}
+
+void    lex_delone(t_lex **lst, int del)
+{
+    t_lex   *node;
+    t_lex   *prev;
+
+    node = *lst;
+    prev = NULL;
+    if (node && node->index == del)
+    {
+        lex_delfirst(lst);
+        return ;
+    }
+    while (node && node->index != del)
+    {
+        prev = node;
+        node = node->next;
+    }
+    if (node)
+    {
+        if (prev)
+            prev->next = node->next;
+        lex_clearone(&node);
+    }
+}
+
 int  find_type(char *literal)
 {
     if ((literal[0] == C_LESS && literal[1] == C_LESS))
@@ -187,10 +282,18 @@ int  find_type(char *literal)
     return (T_WORD);
 }
 
-t_lex	*tokenizer(char *input)
+t_lex   *lex_lstlast(t_lex *tokens)
+{
+    while (tokens->next)
+        tokens = tokens->next;
+    return (tokens);
+}
+
+t_lex	*tokenizer(char *input, t_data *data)
 {
 	t_lex	*tokens;
 	int		length;
+    t_lex   *last;
 
 	length = 0;
 	tokens = NULL;
@@ -201,7 +304,13 @@ t_lex	*tokenizer(char *input)
 		else
 		{
 			length = token_length(input);
-			tokens = fill_tokens(tokens, input, length);
+			tokens = fill_tokens(tokens, input, length, data);
+            last = lex_lstlast(tokens);
+            if (last->index == -1)
+            {
+                lex_delone(&tokens, -1);
+                break ;
+            }
 			input += length;
 		}
 	}
@@ -212,13 +321,16 @@ t_lex	*tokenizer(char *input)
 int main()
 {
     t_lex *tokens;
+    t_data data;
 
-    tokens = tokenizer("ls > out | cat -e << here");
+    data.g_exit = 0;
+    tokens = tokenizer("<< here > > << hey>", &data);
     while (tokens)
     {
         printf("%i ", tokens->index);
         printf("%s ", tokens->literal);
         printf("%i ", tokens->type);
+        printf("error status: %i", data.g_exit);
         printf("\n");
         tokens = tokens->next;
     }
