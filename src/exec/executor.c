13de744fccbe2bb31d44a469d98f84361a1c9eb3
erikgonk <twire01@gmail.com>
2024-07-25 18:14:23 +0200

    exec (update)

--- src/exec/executor.c
+++ src/exec/executor.c
@@ -6,7 +6,7 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:13:37 by erigonza          #+#    #+#             */
-/*   Updated: 2024/07/25 12:42:15 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/07/25 17:56:53 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,12 @@ typedef struct s_exec
 	int			err_status;
 }	t_exec;
 
-static int	ft_cmds(t_data *data, t_exec *exec)
-{
-	
-}
-
 static int	ft_builtins(t_data *data);
 {
 //	static char		**builtins = {"pwd", "echo", "cd", "export", "unset", "env", "exit"};
 	if (ft_strcmp("pwd", data->cmds->cmd[0]) == 0)
 		data->g_exit = ft_pwd(data);
-	else if (ft_strcmp("echo", data->cmds->cmd[0]) == 0)// not done yet
+	else if (ft_strcmp("echo", data->cmds->cmd[0]) == 0)// NOT DONE YET
 		data->g_exit = ft_echo(data);
 	else if (ft_strcmp("cd", data->cmds->cmd[0]) == 0)
 		data->g_exit = ft_cd(data);
@@ -50,36 +45,24 @@ static int	ft_builtins(t_data *data);
 		data->g_exit = ft_unset(data);
 	else if ("env", data->cmds->cmd[0] == 0)
 		data->g_exit = ft_env(data);
-	else if ("exit", data->cmds->cmd[0] == 0)// not done yet
+	else if ("exit", data->cmds->cmd[0] == 0)// NOT DONE YET
 		data->g_exit(data);
 	else
 		return (127);
 	return (data->g_exit);
 }
 
-int	ft_no_pipe_redir(t_data *data, t_exec *exec)
-{
-	if (data->cmds->builtin)// builtin
-		data->g_exit = ft_builtins(data, exec, i);
-	else// cmd
-		data->g_exit = ft_cmds(data, exec);
-	return (data->g_exit);
-// should I put together cmd and the other?
-}
-
 int	ft_executor(t_data	*data, t_exec *exec)
 {
+	if (!data->cmds->next && data->cmds->builtin && data->cmds->redirections)
+		err = ft_builtins(data);
 	while (data->cmds)
 	{
 		if (!data->cmds->cmd || !data->cmds->cmd[exec->i])	
 			return (0);
-		else if (!data->cmds->next)
-			err = ft_no_pipe_redir(data, exec);
-		else if (!no_pipe)
-			err = ft_no_pipe(data, exec);
 		else
-			err = ft_pipe(data, exec);
+			data->g_exit = ft_cmds(data, exec);
 		data->cmds = data->cmds->next;
 	}
-		return (err);
+		return (data->g_exit);
 }
