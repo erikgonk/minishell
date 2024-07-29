9f80121b9d0bebe8e3140bdf2ce699d4bcce4639
erikgonk <twire01@gmail.com>
2024-07-29 11:39:13 +0200

    env update & checker update

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,24 +6,24 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 17:47:31 by erigonza          #+#    #+#             */
-/*   Updated: 2024/07/25 12:03:03 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/07/29 11:36:50 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/builtins.h"
 #include "../../inc/minishell.h"
 
-void    ft_print_env(t_node env)
+void    ft_print_env(t_node *env)
 {
 	while (env)
 	{
 		if (env->var && env->str)
-			ft_printf("%s=%s", env->var, env->str, 1);
+			ft_printf("%s=%s\n", env->var, env->str, 1);
 		env = env->next;
 	}
 }
 
-int	ft_env(t_data data)
+int	ft_env(t_data *data)
 {
 	if (!data->env->start || !data->env->start->var || !data->env->start->str)
 	{
